41167c3d97413b14c5d7724a5b3fc6a2f9edd99d
adelvinc <adriano.delvincio@cern.ch>
2025-11-24 16:27:02 +0100

    wrote functions for 3d simulation

--- toy/UTILS.py
+++ toy/UTILS.py
@@ -9,6 +9,7 @@ mass = 1.6735575e-27 # kg, mass Hbar
 bohr_magneton = 9.2740100657e-24 # J/T, CODATA 2022
 kB = 1.380649e-23   # J/K
 Temperature = 5e-3 # K
+Temperature_transv = 5e-3 # K
 harmonic_degree = 2 # degree of the harmonic potential
 harmonic_coefficient = 10 # coefficient harmonic potential
 Zmin = -0.605 # m
@@ -110,6 +111,73 @@ def InitialCondition(Bfield, CONSTANTS):
     
     return v, Zsample
 
+def BfieldExtended(function, Z, r, CONSTANTS):
+    "3d extent of the magnetic field model, assuming harmonic potential"
+    "Z [m]"
+    "r radius [m]"
+    return function(Z) * (1 + CONSTANTS.harmonic_coefficient*r**CONSTANTS.harmonic_degree)
+
+def InitialCondition_3d(Bfield, CONSTANTS):
+    """
+    Return v(t = 0), z(t = 0)
+    Compute the initial condition of a particle
+    from a maxwell distribution and using the potential of the well
+    this is not a formal calculation, for which you
+    would need microcanonical ensemble formalism
+    """
+    Potential = lambda z: ( .5 * CONSTANTS.bohr_magneton) * Bfield(z) # define the potential
+
+    #---------------------------------------------------------
+    # AXIAL ENERGY
+    #---------------------------------------------------------
+    
+    # extracting the energy from the maxwell distribution
+    scale = np.sqrt(CONSTANTS.kB * CONSTANTS.Temperature/CONSTANTS.mass)
+    
+    Ek_axial = 0
+    while(True):
+        v = maxwell.rvs(scale = scale, size = 1).item()
+        Ek_axial = .5 * CONSTANTS.mass *v*v # Compute kinetic energy
+        if(Ek_axial < (Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid))): # check particle is trapped
+            break 
+    
+    # Now find the corresponding Z at which the U(z) =  Ek_axial
+    #print(Ek_axial, Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid) )
+    
+    Zsol1 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek_axial, 
+                  CONSTANTS.Zmid,
+                  CONSTANTS.Zmax)
+    Zsol2 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek_axial, 
+                  CONSTANTS.Zmin, 
+                  CONSTANTS.Zmid)
+
+    Zsample = np.random.uniform(Zsol1, Zsol2)
+    
+    # compute new kinetic energy
+    Ek_axial = Ek_axial - (Potential(Zsample) - Potential(CONSTANTS.Zmid))
+
+    if(Ek_axial < 0):
+        print(f"Error!!!, Ek_axial {Ek_axial} < 0, fixing it")
+        Ek_axial = 0
+
+    # compute velocity
+    v_axial =  np.sqrt(2* Ek_axial / CONSTANTS.mass)
+    v_axial = v_axial * (np.random.choice([-1, 1]))
+
+    #---------------------------------------------------------
+    # TRANSVERSE ENERGY
+    #---------------------------------------------------------
+    
+    # extracting the energy from the maxwell distribution
+    scale = np.sqrt(CONSTANTS.kB * CONSTANTS.Temperature_transv/CONSTANTS.mass)
+    
+    v_transv = maxwell.rvs(scale = scale, size = 1).item()
+    Ek_transv = .5 * CONSTANTS.mass *v_transv*v_transv # Compute kinetic energy 
+
+    theta = np.random.uniform(0, 2*np.pi)
+    vx, vy = v_transv * np.cos(theta), v_transv * np.sin(theta)
+    
+    return np.array([v_axial, vx, vy], dtype=float), np.array([0, 0, Zsample], dtype=float)
 
 
 #################################################
@@ -127,7 +195,7 @@ def idx_nearest(z, z0, dz, N):
     return idx
 
 @njit
-def dB_seg(z, x, y, 
+def dB_seg(z, x, y,
            Tloc, ramplength, 
            z0, dz, 
            dBinit,
@@ -136,6 +204,7 @@ def dB_seg(z, x, y,
            Bfinal):
     # compute the 3d gradient
     # Tloc = local time during the ramp
+    # z0, dz needed to find the closest point on the grid
     
     alpha = Tloc / ramplength # time parameter
     N = dBinit.shape[0]
@@ -161,50 +230,92 @@ def dB_seg(z, x, y,
     dBt_dx = dBi_dx + (dBf_dx - dBi_dx) * alpha
     dBt_dy = dBi_dy + (dBf_dy - dBi_dy) * alpha
     dBt_dz = dBi_dz + (dBf_dz - dBi_dz) * alpha
-    
-    return dBt_dx, dBt_dy, dBt_dz
+
+    dB = np.array([dBt_dx, dBt_dy, dBt_dz])
+    return dB
 
 @njit
-def dB_plateau(z, z0, dz, dBgrid):
+def dB_plateau(z, x, y, z0, dz, dBgrid, Bgrid):
     N = dBgrid.shape[0]
     i = idx_nearest(z, z0, dz, N)
-    return dBgrid[i]
+
+    r = np.sqrt((x*x + y*y)) # compute radius
+    coeff_z = (1 + harmonic_coefficient * r**harmonic_degree)
+    coeff_x = harmonic_degree*harmonic_coefficient* x**(harmonic_degree - 1)
+    coeff_y = harmonic_degree*harmonic_coefficient* y**(harmonic_degree - 1)
+    
+    # Compute Bfield gradient at alpha = 0, ramp start 
+    dB_dx = dBgrid[i]  * coeff_x
+    dB_dy = Bgrid[i]   * coeff_y
+    dB_dz = Bgrid[i]   * coeff_z
+
+    dB = np.array([dB_dx, dB_dy, dB_dz])
+    
+    return dB
 
 @njit
-def step_all_particles(Z, V, Time, # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-                       seg_id, Tloc, ramp_len,
+def step_all_particles(R,V,
+                       Time,seg_id, 
+                       Tloc, ramp_len,
                        z0, dz,          # da zgrid[0], zgrid[1]-zgrid[0]
-                       dBinit, dBfinal,
+                       dBinit,
+                       Binit,
+                       dBfinal,
+                       Bfinal,
                        Annih, T_ann):
+    # Z, X, Y: 3d array for each particle
     
-    N = Z.shape[0] # Get Number of Particles
-    for i in range(N):
+    N = R.shape[0] # Get Number of Particles
+    for i in range(N): # Lop on particles
         if Annih[i]:
             continue
 
-        z = Z[i]
+        # get coordinates
+        x = R[i, 0]
+        y = R[i, 1]
+        z = R[i, 2]
 
         if(seg_id == 0): # ramp
-            dB = dB_seg(z, Tloc, ramp_len, z0, dz, dBinit, dBfinal) # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            dB = dB_seg(z, x, y,
+                        Tloc, ramp_len, 
+                        z0, dz, 
+                        dBinit,
+                        Binit,
+                        dBfinal,
+                        Bfinal)
         elif(seg_id == 1):
-            dB = dB_plateau(z, z0, dz, dBfinal) # >>>
+            dB = dB_plateau(z, x, y, 
+                            z0, dz, 
+                            dBfinal, Bfinal)
 
         a  = -mu_eff_over_m * dB
 
         # --- Verlet posizione ---
-        z_new = z + V[i]*dt + 0.5*a*dt*dt
-
+        R_new = R[i] + V[i]*dt + 0.5*a*dt*dt
+        # get coordinates
+        x_new = R_new[0]
+        y_new = R_new[1]
+        z_new = R_new[2]
+        
         # --- dB/dz al passo successivo ---
         if seg_id == 0: # ramp
-            dB_next = dB_seg(z_new, Tloc+dt, ramp_len, z0, dz, dBinit, dBfinal) # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            dB_next = dB_seg(z_new, x_new, y_new,
+                             Tloc+dt, ramp_len,
+                             z0, dz,
+                             dBinit, 
+                             Binit,
+                             dBfinal,
+                             Bfinal )
         elif seg_id == 1:
-            dB_next = dB_plateau(z_new, z0, dz, dBfinal)
+            dB_next = dB_plateau(z_new, x_new, y_new, 
+                                 z0, dz, 
+                                 dBfinal)
 
         a_next = -mu_eff_over_m * dB_next
 
-        # --- Verlet velocità ---
+        # --- Update Velocity and Position ---
         V[i] = V[i] + 0.5*(a + a_next)*dt
-        Z[i] = z_new
+        R[i] = R_new
 
         # --- annichilazione se esce ---
         if (z_new < zacceptance_min) or (z_new > zacceptance_max):

a78b849d546d007876cac8f379b1bc21c9eb5e86
adelvinc <adriano.delvincio@cern.ch>
2026-01-28 16:45:51 +0100

    producing the Scurve, not really a great progress

--- toy/UTILS.py
+++ toy/UTILS.py
@@ -277,7 +277,7 @@ def dB_plateau(z, x, y, z0, dz, dBgrid, Bgrid):
     
     return dB_dx, dB_dy, dB_dz
 
-@njit(parallel=True,fastmath=True)
+@njit(parallel=True,fastmath=False)
 def step_all_particles(
     R, V,                             # Coordinates (N,3)
     Time, seg_id, Tloc, ramp_len,     # Time and segment identification
@@ -385,7 +385,7 @@ def step_all_particles(
             V[i, 2] = Matrix[2,0]*vx0 + Matrix[2,1]*vy0 + Matrix[2,2]*vz0
 
 
-@njit(fastmath=True)
+@njit(fastmath=False)
 def evolve_all_particles(R_array, V_array, Time,
         z0, dz,
         Annihilation, Time_Annihilation,

6050b0ca3eb10d379fbdb9d822b8aabe7d36cd93
adelvinc <adriano.delvincio@cern.ch>
2026-02-19 14:07:03 +0100

    last changes

--- toy/UTILS.py
+++ toy/UTILS.py
@@ -47,56 +47,35 @@ def Gradient_Computation_3d(f, x, y, z,  h=0.0005):
     df_dz = (f(z + dz) - f(z - dz)) / (2*h)
     return [df_dx, df_dy, df_dz]
 
-def InitialCondition(Bfield, CONSTANTS):
-    """
-    Return v(t = 0), z(t = 0)
-    Compute the initial condition of a particle
-    from a maxwell distribution and using the potential of the well
-    this is not a formally correct calculation, for which you
-    would need microcanonical ensemble formalism
-    """
-    Potential = lambda z: ( .5 * CONSTANTS.bohr_magneton) * Bfield(z) # define the potential
-    
-    # extracting the energy from the maxwell distribution
-    scale = np.sqrt(CONSTANTS.kB * CONSTANTS.Temperature/CONSTANTS.mass)
-
-    Ek = 0
-    while(True):
-        v = maxwell.rvs(scale = scale, size = 1)
-        Ek = .5 * CONSTANTS.mass *v*v # Compute kinetic energy
-        if(Ek < (Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid))): # check particle is trapped
-            break 
-    
-    # Now find the corresponding Z at which the U(z) =  Ek
-    #print(Ek, Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid) )
-    
-    Zsol1 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek, 
-                  CONSTANTS.Zmid,
-                  CONSTANTS.Zmax)
-    Zsol2 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek, 
-                  CONSTANTS.Zmin, 
-                  CONSTANTS.Zmid)
-
-    Zsample = np.random.uniform(Zsol1, Zsol2)
-    
-    # compute new kinetic energy
-    Ek = Ek - (Potential(Zsample) - Potential(CONSTANTS.Zmid))
-
-    if(Ek < 0):
-        print(f"Error!!!, Ek {Ek} < 0, fixing it")
-        Ek = 0
-
-    # compute velocity
-    v =  np.sqrt(2* Ek / CONSTANTS.mass)
-    v = v * (np.random.choice([-1, 1]))
-    
-    return v, Zsample
 
 def BfieldExtended(function, Z, r, CONSTANTS):
     "3d extent of the magnetic field model, assuming harmonic potential"
     "Z [m]"
     "r radius [m]"
     return function(Z) * (1 + CONSTANTS.harmonic_coefficient*r**CONSTANTS.harmonic_degree)
+
+
+
+# Now find the corresponding Z at which the U(z) =  Ek_axial
+#print(Ek_axial, Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid) )
+# print(Ek_axial)
+# try:
+#     Zsol1 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek_axial, 
+#                   CONSTANTS.Zmid,
+#                   CONSTANTS.Zmax)
+#     Zsol2 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek_axial, 
+#                   CONSTANTS.Zmin, 
+#                   CONSTANTS.Zmid)
+# except:
+#     print("error in brentq")
+#     print(f"{Ek_axial}")
+#     print(f" {Potential(CONSTANTS.Zmin) - Potential(CONSTANTS.Zmid) - Ek_axial}, {Potential(CONSTANTS.Zmid) - Potential(CONSTANTS.Zmid) - Ek_axial}")    
+
+#Zsample = np.random.uniform(Zsol1, Zsol2)
+
+# compute new kinetic energy
+#Ek_axial = Ek_axial - (Potential(Zsample) - Potential(CONSTANTS.Zmid))
+
 
 def InitialCondition_3d(Bfield, CONSTANTS):
     """
@@ -121,28 +100,11 @@ def InitialCondition_3d(Bfield, CONSTANTS):
         Ek_axial = .5 * CONSTANTS.mass *v*v # Compute kinetic energy
         if(Ek_axial < (Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid))): # check particle is trapped
             break 
-    
-    # Now find the corresponding Z at which the U(z) =  Ek_axial
-    #print(Ek_axial, Potential(CONSTANTS.Zmax) - Potential(CONSTANTS.Zmid) )
-    # print(Ek_axial)
-    # try:
-    #     Zsol1 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek_axial, 
-    #                   CONSTANTS.Zmid,
-    #                   CONSTANTS.Zmax)
-    #     Zsol2 = brentq(lambda z: Potential(z) - Potential(CONSTANTS.Zmid) - Ek_axial, 
-    #                   CONSTANTS.Zmin, 
-    #                   CONSTANTS.Zmid)
-    # except:
-    #     print("error in brentq")
-    #     print(f"{Ek_axial}")
-    #     print(f" {Potential(CONSTANTS.Zmin) - Potential(CONSTANTS.Zmid) - Ek_axial}, {Potential(CONSTANTS.Zmid) - Potential(CONSTANTS.Zmid) - Ek_axial}")
-        
-        
 
-    #Zsample = np.random.uniform(Zsol1, Zsol2)
-    
-    # compute new kinetic energy
-    #Ek_axial = Ek_axial - (Potential(Zsample) - Potential(CONSTANTS.Zmid))
+    ####
+    #///
+    ####
+
 
     if(Ek_axial < 0):
         print(f"Error!!!, Ek_axial {Ek_axial} < 0, fixing it")
@@ -162,8 +124,15 @@ def InitialCondition_3d(Bfield, CONSTANTS):
     v_transv = maxwell.rvs(scale = scale, size = 1).item()
     Ek_transv = .5 * CONSTANTS.mass *v_transv*v_transv # Compute kinetic energy 
 
+    #####
+    #####
+    # Overwrite velocity
+    #v_axial  = np.sqrt(3* CONSTANTS.kB * CONSTANTS.Temperature / CONSTANTS.mass)
+    #v_transv = np.sqrt(3* CONSTANTS.kB * CONSTANTS.Temperature_transv / CONSTANTS.mass)
+    
     theta = np.random.uniform(0, 2*np.pi)
     vx, vy = v_transv * np.cos(theta), v_transv * np.sin(theta)
+    
     
     return np.array([vx, vy, v_axial], dtype=float), np.array([0, 0, CONSTANTS.Zmid], dtype=float)
 
